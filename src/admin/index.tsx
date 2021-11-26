edf2f5fce1d2053855e945e9bd8f3f56494c3222
WindDancer <164173925@qq.com>
2021-11-26 17:32:59 +0800

    chore: 多页面应用的配置

---
+++ src/admin/index.tsx
@@ -0,0 +1,16 @@
+/*
+ * @Title: admin后台页面主入口
+ * @Author: huangjitao
+ * @Date: 2021-11-26 15:24:10
+ * @Description: description of this file
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>后台页面主入口</div>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
---
+++ src/home/index.tsx
@@ -0,0 +1,15 @@
+/*
+ * @Title: home前台页面主入口
+ * @Author: huangjitao
+ * @Date: 2021-11-26 15:24:59
+ * @Description: description of this file
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>前台页面主入口</div>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
