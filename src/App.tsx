4bf8c69fb3cc47d0723e1d63eba02bec02d752dd
WindDancer <164173925@qq.com>
2021-11-26 15:12:37 +0800

    feat: 删除无用的文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+/*
+ * @Title: your project
+ * @Author: huangjitao
+ * @Date: 2021-11-26 14:54:59
+ * @Description: description of this file
+ */
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
+/*
+ * @Title: your project
+ * @Author: huangjitao
+ * @Date: 2021-11-26 14:54:59
+ * @Description: description of this file
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +15,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
